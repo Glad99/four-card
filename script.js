1f70ff19c41f59305d0948e727f027a7d72ea9ac
Gladys <gladysalu1@gmail.com>
2023-12-11 12:55:57 +0100

    added js

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("Hey")
+
+//alert
+alert("Hello!")
+
+//prompt
+let visitorName = prompt('What is your name?')
+
+console.log(visitorName)
+
+//confirm
+let firstname = confirm( "Do you want to leave?")
+console.log(firstname )
+
+const anObject = {
+    name: "glad",
+    age: 30,
+    nigerian: true,
+    nationality: ["nigerian", "uk"],
+    car: {
+        brand: "toyota",
+        year: "2023",
+    }
+}
+console.log(anObject);
+
+
+
+
